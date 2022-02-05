f5f6fe5f3c1be37ecac534e15d9bd2eda9e2883f
Maria <vahaidymaria00@gmail.com>
2022-02-05 17:39:06 +0500

    UI

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const { default: axios } = require("axios");
+
+function input() {
+    var p_name = document.getElementById("prodname").value;
+    localStorage.setItem("Pname",p_name);
+
+    var qnt = document.getElementById("quant").value;
+    localStorage.setItem("Quan",qnt)
+    
+    var man = document.getElementById("manufact").value;
+    localStorage.setItem("manuf",man)
+
+    var supli = document.getElementById("supp").value;
+    localStorage.setItem("suppl",supli)
+
+    var amunt = document.getElementById("price").value;
+    localStorage.setItem("amnt",amunt)
+
+    // var details = {'Product_name': p_name, 'Quantity': qnt, 'Manufacturer': man, 'Supplier': supli, 'Amount': amunt}
+
+    axios.get('https://127.0.0.1:5000/chain').then(response => {
+        console.log(response.data);
+    }).catch(error => console.error(error));
+}
